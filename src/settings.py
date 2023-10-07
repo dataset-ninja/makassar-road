b1ff6abf839ad46d5bff36ef9d34302965ce29c7
erastov-alex <erastov.alex@gmail.com>
2023-10-07 12:04:30 +0300

    upd gitignore, options, settings

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/nublanazqalani/semantic-segmentation-makassaridn-road-dataset"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 6797998
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/makassar-road"
